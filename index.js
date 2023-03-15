72f0a19d0f8ebb2f458f6d03163d1765e3a2aca5
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-03-14 22:48:09 -0300

    Video 2 de API - creadas apis de cadastro y consulta

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ const express = require("express");
 
 const server = express();
 
+const products = [];
+let id = 0;
+
 server.use(express.json());
 
-server.get('/', (req, resp)=>{
-    return resp.json({ message: "Hello Node + express!"})
+server.get("/products/:productId", (req, res) => {
+    const { productId } = req.params;
+
+    const product = products.find(p => p.id === Number(productId));
+
+    if(!product){
+        return res.status(400).json({ error: "Produto não encontrado"});
+    }
+
+    return res.json(product);
 })
 
-server.get('/welcome', (req, resp)=>{
-    return resp.json({ message: "Welcome stranger"})
+server.get("/products", (req, res) => {
+    return res.json(products);
 })
+
+server.post("/products", (req, res) => {
+    const { name , price } = req.body;
+
+    products.push({
+        id: ++id,
+        name,
+        price
+    });
+
+    return res.json(products.slice(-1)[0]);
+});
 
 server.listen(3333);
