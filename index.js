de004f77c023835466a85663afd79494cfae5660
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-03-20 15:17:44 -0300

    Added autentication

--- index.js
+++ index.js
@@ -9,10 +9,40 @@ app.get("/", (req, res, next) => {
     res.json({message: "Teste OK!"});
 })
 
-app.get("/clientes", (req, res, next) => {
-    console.log("Retornou os clientes!");
-    res.json([{id:1, nome:'bruno'}]);
+// app.get("/clientes", (req, res, next) => {
+//     console.log("Retornou os clientes!");
+//     res.json([{id:1, nome:'bruno'}]);
+// })
+
+app.post('/login', (req, res, next) => {
+    if(req.body.user === 'bruno' && req.body.password === '123'){
+        const id = 1;
+        const token = jwt.sign({id}, process.env.SECRET, {
+            expiresIn: 300
+        });
+        return res.json({ auth: true, token: token});
+    }
+
+    res.status(500).json({message: 'Login invalido!'});
 })
+
+app.get('/clientes', verifyJWT, (req, res, next) => {
+    console.log("Retornou todos clientes!");
+    res.json([{id: 1, nome: 'bruno'}]);
+})
+
+
+function verifyJWT(req, res, next){
+    const token = req.headers['x-access-token'];
+    if(!token) return res.status(401).json({ auth: false, message: 'No token provided.'});
+
+    jwt.verify(token, process.env.SECRET, function(err, decoded) {
+        if(err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.'});
+
+        req.userId = decoded.id;
+        next();
+    });
+}
 
 const server = http.createServer(app);
 server.listen(3000);
